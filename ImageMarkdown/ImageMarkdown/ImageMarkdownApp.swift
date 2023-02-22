cd5d8d18ed30166de93e816e4d9c4c878669243d
JesusChristOurGod <56786170+JesusChristOurGod@users.noreply.github.com>
2023-02-22 14:40:07 +0300

    Add files via upload

---
+++ ImageMarkdown/ImageMarkdown/ImageMarkdownApp.swift
@@ -0,0 +1,19 @@
+//
+//  ImageMarkdownApp.swift
+//  ImageMarkdown
+//
+//  Created by Горячев Александр on 21.02.2023.
+//
+
+import SwiftUI
+import PythonKit
+@main
+struct ImageMarkdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                //.frame(width:800,height: 600)
+            
+        }
+    }
+}
